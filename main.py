8f792751d62eaca65f325ccef192a12911d6ceec
Bram Ziltzer <bziltzer@gmail.com>
2022-11-23 15:06:30 -0500

    add placeholder functions

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pymysql
 
 def db_connect():
     # prompt for username and password
-    print("Please provide your MySQL username and password to enter the library databse.")
+    print("Please provide your MySQL username and password to enter the library database.")
+
     good_login = False
     while good_login is False:
         # TODO double check login error handling
@@ -15,10 +16,10 @@ def db_connect():
                 password= password,
                 db='library_system', 
                 charset='utf8mb4', 
-                cursorclass=pymysql.cursors.DictCursor
+                cursorclass= pymysql.cursors.DictCursor
             )
             good_login = True
-
+            
         except Exception as e:
             print("Username or password is incorrect.")
             inpt = input("Type Q to quit or any other character to try again: ")
@@ -27,41 +28,81 @@ def db_connect():
         
     return cnx, username
 
-def main():
+def search_books(cursor):
+    pass
 
+def pay_fine(cursor):
+    pass
+
+def manage_members(cursor):
+    pass
+
+def book_checkout(cursor):
+    pass
+
+def book_return(cursor):
+    pass
+
+def add_book(cursor):
+    quit_book_loop = False
+    while quit_book_loop is False:
+        choice = input()
+
+def mainloop():
     # connect to library_system
-    #cnx, username = db_connect()
-    
-    #print(f"Welcome to the inter-library database system, {username}!")
-    quit_main_loop = False
-
+    cnx, username = db_connect()
+    cur = cnx.cursor();
+    print(f"Welcome to the inter-library database system, {username}!")
+   
     options = {
-        1: "Checkout book", 
+        1: "Checkout book",
         2: "Return book", 
-        3: "Search books", 
-        4: "add books to system",
-        5: "pay fine",
-        6: "search all late fees and overdue books",
-        7: "add/edit/remove member",
-        8: "find book at different library",
+        3: "Add book(s) to system",
+        4: "Search books", 
+        5: "View overdue books",
+        6: "View all late fees",
+        7: "Pay fine",
+        8: "View/manage members",
         0: "Quit",
         }
-    
+
+    # main looop
+    quit_main_loop = False
     while quit_main_loop is False:
-        print("What would you like to do? Type the corresponding number to continue.")
+        print("Main menu. Type the corresponding number to continue.")
         for each in options:
             print(str(each) + ": " + options[each])
         choice = int(input("> "))
+
         match choice:
             case 0:
                 quit_main_loop = True
+            case 1:
+                book_checkout(cur)
+            case 2:
+                book_return(cur)
+            case 3:
+                add_book(cur)
+            case 4:
+                search_books(cur)
+            case 5:
+                cur.execute("CALL view_all_overdue_books();")
+            case 6:
+                cur.execute("CALL view_all_late_fees();")
+            case 7:
+                pay_fine(cur)
+            case 8:
+                manage_members(cur)
             case _:
                 print()
-                print(f"{choice} is invalid. Try a different option.")
-                print()
-                input("Press enter to continue.")
+                print(f"{choice} is invalid.")
+                input("Press enter to return to the menu.")
 
-    #cnx.close()
+    cur.close()
+    cnx.close()
+
+def main():
+    mainloop()
 
 if __name__ == "__main__" :
     main()
